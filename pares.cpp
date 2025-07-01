a3493b16695a198c17be7e9a0b57ac4063ebd204
Kaue-Francisco <kauesantos.francisco@gmail.com>
2025-06-30 23:43:53 -0300

    adicionado programa para imprimir números pares de 2 a 100

---
+++ pares.cpp
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+ 
+int main() {
+    
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cout << "2" << endl;
+    cout << "4" << endl;
+    cout << "6" << endl;
+    cout << "8" << endl;
+    cout << "10" << endl;
+    cout << "12" << endl;
+    cout << "14" << endl;
+    cout << "16" << endl;
+    cout << "18" << endl;
+    cout << "20" << endl;
+    cout << "22" << endl;
+    cout << "24" << endl;
+    cout << "26" << endl;
+    cout << "28" << endl;
+    cout << "30" << endl;
+    cout << "32" << endl;
+    cout << "34" << endl;
+    cout << "36" << endl;
+    cout << "38" << endl;
+    cout << "40" << endl;
+    cout << "42" << endl;
+    cout << "44" << endl;
+    cout << "46" << endl;
+    cout << "48" << endl;
+    cout << "50" << endl;
+    cout << "52" << endl;
+    cout << "54" << endl;
+    cout << "56" << endl;
+    cout << "58" << endl;
+    cout << "60" << endl;
+    cout << "62" << endl;
+    cout << "64" << endl;
+    cout << "66" << endl;
+    cout << "68" << endl;
+    cout << "70" << endl;
+    cout << "72" << endl;
+    cout << "74" << endl;
+    cout << "76" << endl;
+    cout << "78" << endl;
+    cout << "80" << endl;
+    cout << "82" << endl;
+    cout << "84" << endl;
+    cout << "86" << endl;
+    cout << "88" << endl;
+    cout << "90" << endl;
+    cout << "92" << endl;
+    cout << "94" << endl;
+    cout << "96" << endl;
+    cout << "98" << endl;
+    cout << "100" << endl;
+}
